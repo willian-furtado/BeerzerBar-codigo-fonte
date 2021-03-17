42bfb4afcf2f7cea2e2264f2bb45901a17e3b52e
willianjoh <willian.silva2016@outlook.com>
2021-03-17 19:13:24 -0300

    FrontEnd- Tela Inicial

---
+++ beerzerBar-web/src/app/template/template.module.ts
@@ -0,0 +1,27 @@
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './../home/home.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+
+@NgModule({
+  declarations: [
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent,
+  ],
+  imports: [
+    RouterModule,
+    CommonModule
+  ],
+  exports: [
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent
+  ]
+})
+export class TemplateModule { }
