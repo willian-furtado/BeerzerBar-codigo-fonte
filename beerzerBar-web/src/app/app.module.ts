48748b35b7b04a230600deb9197b9fa1dbba8b43
willianjoh <willian.silva2016@outlook.com>
2021-03-16 22:57:59 -0300

    FrontEnd

--- beerzerBar-web/src/app/app.module.ts
+++ beerzerBar-web/src/app/app.module.ts
@@ -1,18 +1,20 @@
+import { TemplateModule } from './template/template.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TemplateModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
